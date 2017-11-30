0057252d62c3ea1e969816a049d543ff61c58fdd
Drogebot <drogebot@hotmail.com>
2017-11-30 16:49:34 +0100

    I don't understand music
    No idea how to turn vibration into music

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -35,7 +35,8 @@ public class Turret : MonoBehaviour
             // Rumble for 200 milliseconds, with low frequency rumble at 160 Hz and high frequency rumble at 320 Hz. For more information check:
             // https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/master/rumble_data_table.md
 
-            InputHelper.SetRumble(PlayerID, 160, 320, 0.6f, 10000);
+            StartCoroutine(PlayMario());
+            //InputHelper.SetRumble(PlayerID, 160, 320, 0.6f, 10000);
 
             // The last argument (time) in SetRumble is optional. Call it with three arguments to turn it on without telling it when to turn off.
             // (Useful for dynamically changing rumble values.)
@@ -54,4 +55,70 @@ public class Turret : MonoBehaviour
 		}
 		gameObject.transform.rotation = Quaternion.AngleAxis(rotation, Vector3.back);
 	}
+
+
+    float E3 = 329.628f;//164.814f;
+    float G3 = 391.995f;//195.998f;
+    float A3 = 440;//220f;
+    float Am3 = 466.164f;//233.082f;
+    float B3 = 493.883f;//246.942f;
+    float C4 = 523.251f;//261.626f;
+    float D4 = 587.330f;//293.665f;
+    float E4 = 659.255f;//329.628f;
+    float F4 = 698.456f;//349.228f;
+    float G4 = 783.991f;//391.995f;
+    float A4 = 880;//440;
+
+    bool mario = false;
+    IEnumerator PlayMario()
+    {
+        int speed = 60000/(128*4);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, C4, 320, 1f, speed); //C4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, G4, 320, 1f, speed); //G4
+        yield return new WaitForSeconds(speed * 6 / 1000f);
+        InputHelper.SetRumble(PlayerID, G3, 320, 1f, speed); //G3
+        yield return new WaitForSeconds(speed * 5 / 1000f);
+        InputHelper.SetRumble(PlayerID, C4, 320, 1f, speed); //C4
+        yield return new WaitForSeconds(speed * 4 / 1000f);
+        InputHelper.SetRumble(PlayerID, G3, 320, 1f, speed); //G3
+        yield return new WaitForSeconds(speed * 4 / 1000f);
+        InputHelper.SetRumble(PlayerID, E3, 320, 1f, speed); //E3
+        yield return new WaitForSeconds(speed * 4 / 1000f);
+        InputHelper.SetRumble(PlayerID, A3, 320, 1f, speed); //A3
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, B3, 320, 1f, speed); //B3
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, Am3, 320, 1f, speed); //A#3
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, A3, 320, 1f, speed); //A3
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, G3, 320, 1f, speed); //G3
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, G4, 320, 1f, speed); //G4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, A4, 320, 1f, speed); //A4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, F4, 320, 1f, speed); //F4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, G4, 320, 1f, speed); //G4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, E4, 320, 1f, speed); //E4
+        yield return new WaitForSeconds(speed * 3 / 1000f);
+        InputHelper.SetRumble(PlayerID, C4, 320, 1f, speed); //C4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, D4, 320, 1f, speed); //D4
+        yield return new WaitForSeconds(speed * 2 / 1000f);
+        InputHelper.SetRumble(PlayerID, B3, 320, 1f, speed); //B3
+    }
 }
