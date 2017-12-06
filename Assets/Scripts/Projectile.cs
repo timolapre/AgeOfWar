b6652d596ea4e72505cff1cf93cad9529b8bb615
Drogebot <drogebot@hotmail.com>
2017-12-06 13:21:54 +0100

    Projectile has gravity

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,8 +7,7 @@ public class Projectile : MonoBehaviour {
 	public float direction; // 0 is up
     public string kills = "Enemy";
     public int damage;
-
-    float gravity = 9.81f;
+    
 	float speed = 10;
     float lifeLeft = 1;
 
@@ -22,9 +21,14 @@ public class Projectile : MonoBehaviour {
         lifeLeft -= Time.deltaTime;
         if (lifeLeft <= 0)
             Destroy(gameObject);
-		transform.position = new Vector3(transform.position.x - Mathf.Sin(direction * Mathf.PI / 180) * speed * Time.deltaTime, transform.position.y + Mathf.Cos(direction * Mathf.PI / 180) * speed * Time.deltaTime, transform.position.z);
-        if (Mathf.Abs(transform.position.x) > 50 || Mathf.Abs(transform.position.y) > 50)
-            Destroy(gameObject);
+
+        if (direction < 180)
+            direction++;
+        else if (direction > 180)
+            direction--;
+        transform.rotation = Quaternion.AngleAxis(-direction + 90, Vector3.back);
+
+        transform.position = new Vector3(transform.position.x - Mathf.Sin(direction * Mathf.PI / 180) * speed * Time.deltaTime, transform.position.y + Mathf.Cos(direction * Mathf.PI / 180) * speed * Time.deltaTime, transform.position.z);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
