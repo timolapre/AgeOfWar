899fde2a2daf002387d6ce8f2bf00974461c015a
Timo Lapre <t.lapre@students.uu.nl>
2017-12-06 13:04:28 +0100

    dingen werken weer
    en andere dingen niet jeej

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -15,6 +15,7 @@ public class Base : MonoBehaviour {
     public Text MoneyText;
     public Text XpText;
     public Text GameOverText;
+    public Text WhatTierText;
     public float FirstPlayer;
     public float FirstEnemy;
     public int Money;
@@ -59,6 +60,7 @@ public class Base : MonoBehaviour {
         {
             MoneyText.text = "Money: " + Money;
             XpText.text = "XP: " + XP;
+            WhatTierText.text = "Tier " + WhatTier;
         }
         else
         {
@@ -123,7 +125,10 @@ public class Base : MonoBehaviour {
             tempPlayerScript.WhichUnit = id;
             PlayerList.Add(tempPlayer);
             Money -= UnitCosts[(id-1) / 3, (id - 1) % 3];
+            Debug.Log(UnitCosts[(id - 1) / 3, (id - 1) % 3]);
+            return;
         }
+        return;
     }
     public void Reset()
     {
