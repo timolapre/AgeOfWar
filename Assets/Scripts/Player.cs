7482b2e5abe0c5a66d538e529e13d6e0c86ee1bd
Timo Lapre <t.lapre@students.uu.nl>
2017-12-12 12:28:58 +0100

    Timer Attack
    Attacks every x seconds instead of constant

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,14 @@ public class Player : MonoBehaviour {
     public int WhichUnit;
 
     SpriteRenderer SpriteRenderer;
-    public Sprite Sprite1;
-    public Sprite Sprite2;
-    public Sprite Sprite3;
-    public Sprite Sprite4;
-    public Sprite Sprite5;
-    public Sprite Sprite6;
+    public Sprite Sprite1, Sprite2, Sprite3, Sprite4, Sprite5, Sprite6;
 
     private Base BaseScript;
     private float Closest = 1000;
 
-    public int Health;
-    public int Damage;
-    public int Range;
-    private int Xp = 5;
-    private int Money = 2;
+    public int Health, Damage, Range, Xp, Money;
+    private float AttackEveryXSeconds, AttackAfterXSeconds;
+    private int GetDamage;
 
     // Use this for initialization
     void Start () {
@@ -48,7 +41,7 @@ public class Player : MonoBehaviour {
         else if (AtOtherBase && !Colliding && BaseScript.Playing)
             BaseScript.EnemyBaseHealth -= Damage;
         else if (BaseScript.Playing)
-            Attackee.GetComponent<Enemy>().TakeDamage(Damage);
+            //Attackee.GetComponent<Enemy>().StartTakingDamage(Damage, AttackAfterXSeconds, AttackEveryXSeconds);
 
         if (Health <= 0)
         {
@@ -90,12 +83,14 @@ public class Player : MonoBehaviour {
                 Closest = OtherGameObject.transform.position.x;*/
     }
 
-    void OnTriggerStay2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if(transform.position.x < collision.gameObject.transform.position.x && (collision.tag == "Enemy" || collision.tag == "Player"))
             Colliding = true;
         if (collision.tag == "Enemy")
-            Attackee = collision.gameObject;
+            collision.gameObject.GetComponent<Enemy>().StartTakingDamage(Damage, AttackAfterXSeconds, AttackEveryXSeconds);
+        //Attackee = collision.gameObject;
+        Debug.Log(collision.gameObject.name);
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -104,9 +99,15 @@ public class Player : MonoBehaviour {
         Attackee = null;
     }
 
-    public void TakeDamage(int damage)
+    public void StartTakingDamage(int Damage, float GetHitAfterSeconds, float GetHitEverySeconds)
     {
-        Health -= damage;
+        GetDamage = Damage;
+        InvokeRepeating("TakeDamage", GetHitAfterSeconds, GetHitEverySeconds);
+    }
+
+    void TakeDamage()
+    {
+        Health -= GetDamage;
     }
 
     void GetStartValues(int id)
@@ -114,67 +115,79 @@ public class Player : MonoBehaviour {
         if (id == 1)
         {
             transform.localScale = new Vector3(6, 6, 1);
-            Health = 100;
+            Health = 10;
             Damage = 1;
             Range = 1;
             SpriteRenderer.sprite = Sprite1;
             Xp = 1;
             Money = 1;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
 
         if (id == 2)
         {
             transform.localScale = new Vector3(6, 6, 1);
-            Health = 200;
+            Health = 20;
             Damage = 2;
             Range = 1;
             SpriteRenderer.sprite = Sprite2;
             Xp = 2;
             Money = 2;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
 
         if (id == 3)
         {
             transform.localScale = new Vector3(1, 1, 1);
-            Health = 300;
+            Health = 30;
             Damage = 2;
             Range = 1;
             SpriteRenderer.sprite = Sprite3;
             Xp = 3;
             Money = 3;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
 
         if (id == 4)
         {
             transform.localScale = new Vector3(2, 2, 1);
-            Health = 250;
+            Health = 25;
             Damage = 2;
             Range = 1;
             SpriteRenderer.sprite = Sprite4;
             Xp = 4;
             Money = 4;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
 
         if (id == 5)
         {
             transform.localScale = new Vector3(2, 2, 1);
-            Health = 400;
+            Health = 40;
             Damage = 4;
             Range = 1;
             SpriteRenderer.sprite = Sprite5;
             Xp = 5;
             Money = 5;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
 
         if (id == 6)
         {
             transform.localScale = new Vector3(2, 2, 1);
-            Health = 500;
+            Health = 50;
             Damage = 5;
             Range = 1;
             SpriteRenderer.sprite = Sprite6;
             Xp = 5;
             Money = 5;
+            AttackAfterXSeconds = 0.3f;
+            AttackEveryXSeconds = 1f;
         }
     }
 }
