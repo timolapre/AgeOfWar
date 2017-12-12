f08f4a07e7036857a3f455807a0382559dff992e
YouriRombouts <32095006+YouriRombouts@users.noreply.github.com>
2017-12-12 14:22:34 +0100

    Init

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-
+    public Base BaseScript;
+    
 	public float direction; // 0 to the right
     public string kills = "Enemy";
     public int damage;
@@ -16,21 +17,26 @@ public class Projectile : MonoBehaviour {
 
 	void Start()
     {
+        BaseScript = GetComponent<Base>();
         transform.rotation = Quaternion.AngleAxis(-direction, Vector3.back);
 		verspeed = Mathf.Cos(direction * Mathf.PI / 180) * speed;
 		horspeed = -Mathf.Sin(direction * Mathf.PI / 180) * speed;
 	}
 
     // Update is called once per frame
-    void Update () {
-        if (transform.position.y < -5)
-            Destroy(gameObject);
+    void Update ()
+    {
+        if (!BaseScript.Paused)
+        {
+            if (transform.position.y < -5)
+                Destroy(gameObject);
 
-		verspeed += gravity * Time.deltaTime;
-		direction = Mathf.Atan(verspeed / horspeed) * 180 / Mathf.PI;
-		transform.rotation = Quaternion.AngleAxis(-direction + 90 * (horspeed <= 0 ? -1 : 1), Vector3.back);
+            verspeed += gravity * Time.deltaTime;
+            direction = Mathf.Atan(verspeed / horspeed) * 180 / Mathf.PI;
+            transform.rotation = Quaternion.AngleAxis(-direction + 90 * (horspeed <= 0 ? -1 : 1), Vector3.back);
 
-		transform.position = new Vector3(transform.position.x + horspeed * Time.deltaTime, transform.position.y + verspeed * Time.deltaTime, transform.position.z);
+            transform.position = new Vector3(transform.position.x + horspeed * Time.deltaTime, transform.position.y + verspeed * Time.deltaTime, transform.position.z);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
