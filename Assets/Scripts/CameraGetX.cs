adf868cdfc0efb0229340a70009d95ce003f20a0
Timo Lapre <t.lapre@students.uu.nl>
2017-12-13 11:35:58 +0100

    exlposion pausing

--- Assets/Scripts/CameraGetX.cs
+++ Assets/Scripts/CameraGetX.cs
@@ -7,7 +7,7 @@ public class CameraGetX : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         float Camerax = PlayerPrefs.GetFloat("CameraX");
-        transform.position = new Vector3(Camerax,0,0);
+        transform.position = new Vector3(Camerax,0,-9);
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
 
+    public Base BaseScript;
+    public Sprite DestroySprite;
+
 	// Use this for initialization
 	void Start () {
-        Invoke("Destroy", 1.47f);
+        BaseScript = GetComponentInParent<Base>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (BaseScript.Paused)
+            GetComponent<Animator>().enabled = false;   
+        else
+            GetComponent<Animator>().enabled = true;
+
+        if (GetComponent<SpriteRenderer>().sprite == DestroySprite)
+            Destroy();
+    }
 
     void Destroy()
     {
-        Destroy(gameObject);
+        Destroy(gameObject, 0.08f);
     }
 }
