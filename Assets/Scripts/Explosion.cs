63b720cbc4e5a2b21f6173240cf40bf48264c140
Timo Lapre <t.lapre@students.uu.nl>
2017-12-13 11:41:56 +0100

    .

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
 
-    public Base BaseScript;
-    public Sprite DestroySprite;
-
 	// Use this for initialization
 	void Start () {
-        BaseScript = GetComponentInParent<Base>();
+        Invoke("Destroy", 1.47f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (BaseScript.Paused)
-            GetComponent<Animator>().enabled = false;   
-        else
-            GetComponent<Animator>().enabled = true;
-
-        if (GetComponent<SpriteRenderer>().sprite == DestroySprite)
-            Destroy();
-    }
+		
+	}
 
     void Destroy()
     {
-        Destroy(gameObject, 0.08f);
+        Destroy(gameObject);
     }
 }
