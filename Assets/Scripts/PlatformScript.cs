879a8989ac7616509ce4a37f88cd1d8d0fa7313a
YouriRombouts <32095006+YouriRombouts@users.noreply.github.com>
2018-01-14 17:15:41 +0100

    Platform script
    Changes platform to the platform of the right faction.

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+    private Base BaseScript;
+    public int Id;
+    SpriteRenderer SpriteRenderer;
+    // Use this for initialization
+    void Start ()
+    {
+        BaseScript = GetComponentInParent<Base>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Id == 1)
+        {
+            SpriteRenderer.sprite = Resources.Load(BaseScript.WhatFaction + "/Bases/Platform", typeof(Sprite)) as Sprite;
+        }
+        else if (Id == 2)
+        {
+            SpriteRenderer.sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Bases/Platform", typeof(Sprite)) as Sprite;
+        }        
+    }
+}
