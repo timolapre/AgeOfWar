a6ed3294609341f505a01eee59e09aeb56535d14
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-01-16 13:46:04 +0100

    pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+    public Base BaseScript;
+    public void Start()
+    {
+        BaseScript =  GameObject.Find("PBase /main object").GetComponent<Base>();
+    }
+    public void Buttons (string Button)
+    {
+        if (Button == "Resume")
+        {
+            SceneManager.UnloadSceneAsync("Paused");
+        }
+        if (Button == "Restart")
+        {
+            if (SceneManager.GetSceneByName("GameOver") == SceneManager.GetActiveScene())
+            {
+                SceneManager.LoadScene("AgeOfWar", LoadSceneMode.Single);
+            }
+            else
+            {
+                SceneManager.UnloadSceneAsync("Paused");
+                BaseScript.Reset();
+            }
+        }
+        if (Button == "Options")
+        {
+            SceneManager.UnloadSceneAsync("Paused");
+            PlayerPrefs.SetInt("InGame", 1);
+            SceneManager.LoadScene("Options", LoadSceneMode.Additive);
+        }
+        if (Button == "ExitToMenu")
+        {
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+        if (Button == "ExitToDesktop")
+        {
+            Application.Quit();
+        }
+    }
+}
