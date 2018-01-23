6053960e99d160865291341f714503e23d801cdd
Youri Rombouts <youri.rombouts@gmail.com>
2018-01-23 16:02:58 +0100

    GameOverText fix

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class GameoverScript : MonoBehaviour {
+public class GameOverScript : MonoBehaviour
+{
     // Use this for initialization
     public Text WinnerText;
-	void Start ()
+    void Start()
     {
         WinnerText.text = PlayerPrefs.GetString("WinnerString") + " wins";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
