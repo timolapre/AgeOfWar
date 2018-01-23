e71c93c94756f75732dbb127eb65879c6c289c13
Timo Lapre <t.lapre@students.uu.nl>
2018-01-23 21:34:39 +0100

    another one met shit

--- Assets/Scripts/SpawnTimer.cs
+++ Assets/Scripts/SpawnTimer.cs
@@ -65,27 +65,27 @@ public class SpawnTimer : MonoBehaviour {
             }
 
             if (EnemyBaseScript.SpawnList.Count >= 1)
-                Sprite1.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFaction + "/Units/" + EnemyBaseScript.SpawnList[0][1].ToString() + "_" + EnemyBaseScript.SpawnList[0][0].ToString(), typeof(Sprite)) as Sprite;
+                Sprite1.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Units/" + EnemyBaseScript.SpawnList[0][1].ToString() + "_" + EnemyBaseScript.SpawnList[0][0].ToString(), typeof(Sprite)) as Sprite;
             else
                 Sprite1.GetComponent<SpriteRenderer>().sprite = null;
 
             if (EnemyBaseScript.SpawnList.Count >= 2)
-                Sprite2.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFaction + "/Units/" + EnemyBaseScript.SpawnList[1][1].ToString() + "_" + EnemyBaseScript.SpawnList[1][0].ToString(), typeof(Sprite)) as Sprite;
+                Sprite2.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Units/" + EnemyBaseScript.SpawnList[1][1].ToString() + "_" + EnemyBaseScript.SpawnList[1][0].ToString(), typeof(Sprite)) as Sprite;
             else
                 Sprite2.GetComponent<SpriteRenderer>().sprite = null;
 
             if (EnemyBaseScript.SpawnList.Count >= 3)
-                Sprite3.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFaction + "/Units/" + EnemyBaseScript.SpawnList[2][1].ToString() + "_" + EnemyBaseScript.SpawnList[2][0].ToString(), typeof(Sprite)) as Sprite;
+                Sprite3.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Units/" + EnemyBaseScript.SpawnList[2][1].ToString() + "_" + EnemyBaseScript.SpawnList[2][0].ToString(), typeof(Sprite)) as Sprite;
             else
                 Sprite3.GetComponent<SpriteRenderer>().sprite = null;
 
             if (EnemyBaseScript.SpawnList.Count >= 4)
-                Sprite4.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFaction + "/Units/" + EnemyBaseScript.SpawnList[3][1].ToString() + "_" + EnemyBaseScript.SpawnList[3][0].ToString(), typeof(Sprite)) as Sprite;
+                Sprite4.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Units/" + EnemyBaseScript.SpawnList[3][1].ToString() + "_" + EnemyBaseScript.SpawnList[3][0].ToString(), typeof(Sprite)) as Sprite;
             else
                 Sprite4.GetComponent<SpriteRenderer>().sprite = null;
 
             if (EnemyBaseScript.SpawnList.Count >= 5)
-                Sprite5.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFaction + "/Units/" + EnemyBaseScript.SpawnList[4][1].ToString() + "_" + EnemyBaseScript.SpawnList[4][0].ToString(), typeof(Sprite)) as Sprite;
+                Sprite5.GetComponent<SpriteRenderer>().sprite = Resources.Load(BaseScript.WhatFactionEnemy + "/Units/" + EnemyBaseScript.SpawnList[4][1].ToString() + "_" + EnemyBaseScript.SpawnList[4][0].ToString(), typeof(Sprite)) as Sprite;
             else
                 Sprite5.GetComponent<SpriteRenderer>().sprite = null;
         }
